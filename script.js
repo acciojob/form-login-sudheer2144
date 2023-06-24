fd7f2add1eb09389b63093884e2f52d8859a1dfa
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-24 10:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let f = document.getElementsByName("fname");
+	let l = document.getElementsByName("lname");
+	let res=f[0].value +" "+ l[0].value;
 
+	alert(res);
 }
